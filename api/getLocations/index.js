e0756b52436369085fdb06238ae084a8ab9bb71b
onemoresn <onemoresn@gmail.com>
2026-05-12 03:18:04 -0400

    fix: use Cosmos SDK in getLocations for resilient error handling

--- api/getLocations/index.js
+++ api/getLocations/index.js
@@ -1,3 +1,16 @@
-module.exports = async function (context, req, documents) {
-  context.res = { body: documents || [] };
+const { CosmosClient } = require('@azure/cosmos');
+
+const client    = new CosmosClient(process.env.COSMOS_DB_CONNECTION);
+const container = client.database('locationmap').container('locations');
+
+module.exports = async function (context, req) {
+  try {
+    const { resources } = await container.items
+      .query('SELECT c.id, c.sessionId, c.displayName, c.lat, c.lon, c.locationKey, c.updatedAt FROM c')
+      .fetchAll();
+    context.res = { body: resources || [] };
+  } catch (err) {
+    context.log.error('getLocations error:', err.message);
+    context.res = { body: [] };
+  }
 };
